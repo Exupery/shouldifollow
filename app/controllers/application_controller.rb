15242a08902447dde07fa80accb5e0b74de62ed3
Matthew Frost <mf@matthewfrost.com>
2013-01-26 10:27:40 -0500

    set home route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def home
+		@title = "Should I Follow?"
+		@description = "Displays average tweets per day for a Twitter user"
+	end
 end
