f69b55dd5ca28c8e43b1ad19a2f522cc6ff85c10
Matthew Frost <mf@matthewfrost.com>
2013-01-27 16:20:37 -0500

    error handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-
-	def get_stats uname
-		 
-		logger.debug(uname)
-		tw = Twitterer.new(uname)
-		return tw
-	end
-
-	def get_user
-		url = "https://api.twitter.com/1/users/lookup.json?screen_name=frostmatthew&include_entities=true"
-	end
 end
--- app/models/twitterer.rb
+++ app/models/twitterer.rb
@@ -1,7 +1,6 @@
-require 'net/http'
-require 'open-uri'
-require 'json'
-require 'date'
+require "open-uri"
+require "json"
+require "date"
 
 class Twitterer
 	@@user_url = "https://api.twitter.com/1/users/show.json?screen_name="
@@ -16,26 +15,39 @@ class Twitterer
 	end
 
 	def update_stats
-		json = JSON.parse(open(@@user_url+@uname).read)
-		puts json 	#DELME
-		puts "*************"	#DELME
-		@id = json["id_str"]
-		puts json["id_str"]		#DELME
-		puts json["created_at"]	#DELME
-		@allpd = calc_allpd json["statuses_count"], json["created_at"]
+		#json = JSON.parse(open(@@user_url+@uname).read)	#REVERT
+		#json = JSON.parse(open("http://127.0.0.1/user.json").read)
+		#json = JSON.parse(open("https://api.twitter.com/1/users/show.json?screen_name=zzzfrostmatthew").read)
+		json = JSON.parse(open("http://127.0.0.1/error.json").read)
+		if !json || json["errors"]
+			@error = generate_error json
+		else
+			@id = json["id_str"]
+			@allpd = calc_allpd json["statuses_count"], json["created_at"]
+		end
 	end
 
 	def calc_allpd count, since
-		puts count
-		puts since
-		puts "*************"	#DELME
-		puts Date.parse(since)
-		puts Date.today
-		return (count / (Date.today-Date.parse(since))).to_f.round(1)
+		count = 0 if !count
+		days = (since) ? Date.today-Date.parse(since) : 1;
+		return (count / days).to_f.round(1)
+	end
+
+	def generate_error errors
+		err = errors["errors"][0] if errors["errors"][0]
+		if err
+			code = err["code"] if err["code"]
+			if code && (code==88 || code==420)
+				"Whoa! shouldifollow seems to have hit the Twitter API hourly rate limit."
+			elsif err["message"] 
+				err["message"] 
+			end
+		else
+			"Whoops, something went wrong :-("
+		end
 	end
 
 	def error
-		#@error = "Whoa! shouldifollow seems to have hit the Twitter API hourly rate limit."
 		@error
 	end
 
