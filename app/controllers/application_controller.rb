3f793d0a056710774890e23905b13c36a912cd2c
Matthew Frost <mf@matthewfrost.com>
2013-01-26 19:13:51 -0500

    including helper in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   protect_from_forgery
 
   def home
 	uname = params[:uname];
 	@title = (uname) ? "Should I Follow #{uname}?" : "Should I Follow?"
 	@description = "Displays average tweets per day for a Twitter user"
+	if uname
+		get_stats(uname)
+	else
+		toggle_expl
+	end
   end
 
   def search
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def get_stats user
+		logger.debug "get stats for #{user}"
+	end
+
+	def toggle_expl
+		logger.debug "not getting stats"
+	end
 end
