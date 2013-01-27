641748e0c6e3e8641ea4fee22e80bfe9dc157d62
Matthew Frost <mf@matthewfrost.com>
2013-01-27 09:53:40 -0500

    added rate limit error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,12 @@ module ApplicationHelper
 
 	def get_stats user
 		stats = Hash.new(0.0)
+		stats["error"] = nil
 		stats["tpd"] = 1.4
 		stats["rtpd"] = 8
 		stats["trtpd"] = stats["tpd"] + stats["rtpd"]
 		stats["totalpd"] = 20
+		stats["error"] = "Whoa! shouldifollow seems to have hit the Twitter API hourly rate limit."
 		return stats
 	end
 
