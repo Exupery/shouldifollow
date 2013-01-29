33f1d74fcefd0ef97932322f0c852b63680011ea
Matthew Frost <mf@matthewfrost.com>
2013-01-28 19:13:11 -0500

    updated og tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def home
-	uname = params[:uname];
-  uname.gsub!(/@/, '') if uname
-	@title = (uname) ? "Should I Follow @#{uname}?" : "Should I Follow?"
-	@description = "Displays average tweets per day for a Twitter user"
+  	uname = params[:uname];
+    if uname
+      uname.gsub!(/[@<>]/, '')
+      @title = "Should I Follow @#{uname}?"
+      @description = "Displays average tweets per day for @#{uname}"
+    else
+      @title = "Should I Follow?"
+      @description = "Displays average tweets per day for a Twitter user"
+    end
   end
 
   def search
