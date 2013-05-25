e0f0e3b2bb015feeace79660a00d7bcb09fe35ec
Matthew Frost <mf@matthewfrost.com>
2013-05-25 18:15:49 -0400

    added test setups

--- test/unit/twitterer_test.rb
+++ test/unit/twitterer_test.rb
@@ -1,8 +1,18 @@
-require 'test_helper'
+require "test_helper"
  
 class TwittererTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    @tw = Twitterer.new("nasa")
+  end
+
+  test "calcs tweets per day" do
+  	tpd = @tw.tweets_per_day "week"
+  	puts @tw.error
+  	puts ">>#{ENV["TWITTER_CONSUMER_KEY"]}<<>>#{ENV["TWITTER_CONSUMER_SECRET"]}<<>>#{ENV["TWITTER_ACCESS_KEY"]}<<>>#{ENV["TWITTER_ACCESS_SECRET"]}<<"	#DELME
+  	puts tpd
+  	puts tpd.class.name
+    assert !tpd.nil?, "tweets per day is NIL"
+    assert tpd > 0, "tweets per day is NOT greater than zero"
   end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,10 +1,13 @@
-require 'test_helper'
+require "test_helper"
  
 class UserTest < ActiveSupport::TestCase
-  user = User.new
+
+  def setup
+    @user = User.new
+  end
 
   test "oauth token generated" do
-    assert !user.client.nil?, "oauth client is NIL"
-    assert user.client.kind_of?(OAuth::AccessToken), "user.client is NOT OAuth::AccessToken"
+    assert !@user.client.nil?, "oauth client is NIL"
+    assert @user.client.kind_of?(OAuth::AccessToken), "user.client is NOT OAuth::AccessToken"
   end
 end
