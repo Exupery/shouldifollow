807f396dee2fdf22f85fed3b54849adae6ef4444
Matthew Frost <mf@matthewfrost.com>
2013-05-25 11:41:39 -0400

    added test for user.client returns OAuth::AccessToken

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
  
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  user = User.new
+
+  test "oauth token generated" do
+    assert !user.client.nil?, "oauth client is NIL"
+    assert user.client.kind_of?(OAuth::AccessToken), "user.client is NOT OAuth::AccessToken"
   end
 end
