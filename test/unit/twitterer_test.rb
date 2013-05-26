4fda2f8bac11a472d9e5b8f4c0bf6b43913a0845
Matthew Frost <mf@matthewfrost.com>
2013-05-26 10:13:15 -0400

    moved setup assignment to class var

--- test/unit/twitterer_test.rb
+++ test/unit/twitterer_test.rb
@@ -2,46 +2,44 @@ require "test_helper"
  
 class TwittererTest < ActiveSupport::TestCase
 
-  def setup
-    @tw = Twitterer.new("nasa")
-  end
+  @@tw = Twitterer.new("nasa")
 
   test "calcs tweets per day" do
-  	tpd = @tw.tweets_per_day "week"
+  	tpd = @@tw.tweets_per_day "week"
     assert !tpd.nil?, "tweets per day is NIL"
     assert tpd.kind_of?(Numeric), "tweets per day is NOT a number"
     assert tpd > 0, "tweets per day is NOT greater than zero"
   end
 
   test "calcs retweets per day" do
-  	rtpd = @tw.retweets_per_day "week"
+  	rtpd = @@tw.retweets_per_day "week"
     assert !rtpd.nil?, "retweets per day is NIL"
     assert rtpd.kind_of?(Numeric), "retweets per day is NOT a number"
     assert rtpd > 0, "retweets per day is NOT greater than zero"
   end
 
   test "calcs combined per day" do
-  	cpd = @tw.combined_per_day "week"
+  	cpd = @@tw.combined_per_day "week"
     assert !cpd.nil?, "combined per day is NIL"
     assert cpd.kind_of?(Numeric), "combined per day is NOT a number"
     assert cpd > 0, "combined per day is NOT greater than zero"
   end
 
   test "returns all per day" do
-  	apd = @tw.all_per_day
+  	apd = @@tw.all_per_day
     assert !apd.nil?, "all per day is NIL"
     assert apd.kind_of?(Numeric), "all per day is NOT a number"
     assert apd > 0, "all per day is NOT greater than zero"
   end
 
   test "returns joined date" do
-  	joined = @tw.joined
+  	joined = @@tw.joined
   	assert !joined.nil?, "joined date is NIL"
   	assert !joined.empty?, "joined date is empty string"
   end
 
   test "returns most recent tweet" do
-  	tweet = @tw.get_recent_tweet_html
+  	tweet = @@tw.get_recent_tweet_html
   	assert !tweet.nil?, "recent tweet is NIL"
   	assert !tweet.empty?, "recent tweet is empty string"
   end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,12 +2,10 @@ require "test_helper"
  
 class UserTest < ActiveSupport::TestCase
 
-  def setup
-    @user = User.new
-  end
+  @@user = User.new
 
   test "oauth token generated" do
-    assert !@user.client.nil?, "oauth client is NIL"
-    assert @user.client.kind_of?(OAuth::AccessToken), "user.client is NOT OAuth::AccessToken"
+    assert !@@user.client.nil?, "oauth client is NIL"
+    assert @@user.client.kind_of?(OAuth::AccessToken), "user.client is NOT OAuth::AccessToken"
   end
 end
