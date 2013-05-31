f977b5856195b77da9011bfde937809189c10623
Matthew Frost <mf@matthewfrost.com>
2013-05-30 20:16:42 -0400

    added username as placeholder

--- app/assets/javascripts/shouldi.js
+++ app/assets/javascripts/shouldi.js
@@ -1,28 +1,10 @@
 $(document).ready(function() {
-	$('#username').val('username');
-
-	$('#username').focus(function(event) {
-		$('#username').css({
-			'color': '#000000', 
-			'font-style': 'normal'
-		});
-		if ($('#username').val() == 'username') {
-			$('#username').val('');
-		} else {
-			$(this).select();
-		}
-	}).mouseup(function(e) {
-		e.preventDefault();
+	$("#username").focus(function(event) {
+		$(this).attr("placeholder", "");
 	});
 
-	$('#username').blur(function(event) {
-		if ($('#username').val() == '') {
-			$('#username').val('username');
-			$('#username').css({
-				'color': '#959595', 
-				'font-style': 'italic'
-			});
-		}
+	$("#username").blur(function(event) {
+		$(this).attr("placeholder", "username");
 	});
 
 });
