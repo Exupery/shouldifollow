e0619e2b934d7e8263707376369fabad9d01a3b0
Matthew Frost <mf@matthewfrost.com>
2013-06-01 18:47:47 -0400

    added ratelimit route comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Shouldifollow::Application.routes.draw do
 	root :to => "application#home"
 	match "/:uname" => "application#home"
 	match "/search/should-i-follow" => "application#search"
-	match "/dev/ratelimit" => "application#ratelimit"
-	match "/*a" => "application#notfound" #pro 404 handling here
+	match "/dev/ratelimit" => "application#ratelimit"	#shows rate limit data (dev mode only)
+	match "/*a" => "application#notfound" 						#pro 404 handling here
 end
