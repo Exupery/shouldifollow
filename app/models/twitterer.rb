3ba6d16cb8554441ce92384bee7cd1c0f2dbf761
Matthew Frost <mf@matthewfrost.com>
2013-06-02 16:44:41 -0400

    all tests pass

--- app/models/twitterer.rb
+++ app/models/twitterer.rb
@@ -5,18 +5,17 @@ require "timeout"
 
 class Twitterer
 	#REVERT 
-	#@@user_url = "https://api.twitter.com/1.1/users/show.json?screen_name="
-	#@@tweet_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&include_rts=1&screen_name="
-	#@@oembed_url = "https://api.twitter.com/1.1/statuses/oembed.json?maxwidth=500&id="
+	@@user_url = "https://api.twitter.com/1.1/users/show.json?screen_name="
+	@@tweet_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&include_rts=1&screen_name="
+	@@oembed_url = "https://api.twitter.com/1.1/statuses/oembed.json?maxwidth=500&id="
 	#REVERT 
 	#DELME
-	Rails.logger.warn "USING LOCAL RESOURCES"
-	@@user_url = "http://127.0.0.1/user.json?screen_name="
-	@@tweet_url = "http://127.0.0.1/timeline.json?screen_name="
-	@@oembed_url = "http://127.0.0.1/oembed.json?id="
+	#@@user_url = "http://127.0.0.1/user.json?screen_name="
+	#@@tweet_url = "http://127.0.0.1/timeline.json?screen_name="
+	#@@oembed_url = "http://127.0.0.1/oembed.json?id="
 	#DELME
 
-	@@rate_limit_err = "Whoa! shouldifollow seems to have hit the Twitter API hourly rate limit."
+	@@rate_limit_err = "Whoa! shouldifollow seems to have hit Twitter's API rate limit."
 	@@no_user_err = "Username not found"
 	@@gen_err = "Whoops, something went wrong :-("
 	@@timeout_err = "OH NOES - looks likes there was some trouble accessing the Twitter API :-("
