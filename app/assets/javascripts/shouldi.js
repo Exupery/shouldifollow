1432353b5460c3b469bcd25a301e1b0926798360
Matthew Frost <mf@matthewfrost.com>
2013-06-02 21:50:21 -0400

    timezone adjusting

--- app/assets/javascripts/shouldi.js
+++ app/assets/javascripts/shouldi.js
@@ -7,11 +7,12 @@ $(document).ready(function() {
 		$(this).attr("placeholder", "username");
 	});
 
+	update_headers();
 	update_times(0);
 
 });
 
-function update_times(offset) {
+function update_headers() {
 	$(".time-header").each( function() {
 		var h = $(this).attr("data-utc");
 		if (h == 0) {
@@ -27,4 +28,21 @@ function update_times(offset) {
 			$(this).attr("title", h+"AM");
 		}
 	}); 
+}
+
+function update_times(offset) {
+	$(".time-data").each( function() {
+		var h = $(this).attr("data-utc");
+		var adj = 0;
+		if (h+offset < 0) {
+			adj = 25 + offset;
+		} else if (h+offset > 23) {
+			adj = 0 + offset;
+		} else {
+			adj = h + offset;
+		}
+		var v = $("#"+$(this).attr("data-day")+"-utc"+adj).attr("data-base");
+		console.log("#"+$(this).attr("data-day")+"-utc"+adj);	//DELME
+		console.log("h"+h+" w/offset "+offset+" = adj "+adj+" for v="+v);	//DELME
+	});
 }
