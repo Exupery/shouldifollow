0cf7c096629cc416ea27b6ab8576232f89d8ad5d
Matthew Frost <mf@matthewfrost.com>
2013-06-02 20:52:03 -0400

    adjust header/tooltip hour display

--- app/assets/javascripts/shouldi.js
+++ app/assets/javascripts/shouldi.js
@@ -7,4 +7,24 @@ $(document).ready(function() {
 		$(this).attr("placeholder", "username");
 	});
 
+	update_times(0);
+
 });
+
+function update_times(offset) {
+	$(".time-header").each( function() {
+		var h = $(this).attr("data-utc");
+		if (h == 0) {
+			$(this).text("M");
+			$(this).attr("title", "midnight");
+		} else if (h == 12) {
+			$(this).text("N");
+			$(this).attr("title", "noon");
+		} else if (h > 12) {
+			$(this).text(h-12);
+			$(this).attr("title", (h-12)+"PM");
+		} else {
+			$(this).attr("title", h+"AM");
+		}
+	}); 
+}
