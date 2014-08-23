052d82b73305202c70bb26845e85d7b840a39b83
Matthew Frost <mf@matthewfrost.com>
2014-08-23 13:37:22 -0400

    set max-age for static resources

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,8 @@ Shouldifollow::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=2592000"
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
