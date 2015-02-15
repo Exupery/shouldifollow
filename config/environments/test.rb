97fff92667eb9819ea11211b4c8805e1142d29ff
Matthew Frost <mf@matthewfrost.com>
2015-02-15 15:16:41 -0500

    set test order to random

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,4 +30,6 @@ Shouldifollow::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.active_support.test_order = :random
 end
