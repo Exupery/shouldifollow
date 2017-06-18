7c42353d41c41bbc9d8f08163e1594d641a4ab25
Matthew Frost <mf@matthewfrost.com>
2017-06-18 10:19:18 -0400

    correct redirect test params

--- test/functional/application_controller_test.rb
+++ test/functional/application_controller_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class ApplicationControllerTest < ActionController::TestCase
 
   test "should get home" do
-  	get :home
-  	assert_response :success
+    get :home
+    assert_response :success
   end
 
   test "should get ratelimit" do
@@ -13,25 +13,25 @@ class ApplicationControllerTest < ActionController::TestCase
   end
 
   test "search for SEARCH_FOR should redirect to /SEARCH_FOR" do
-  	search_for = "foobar"
-  	get :search, :uname => search_for
-  	assert_redirected_to "/#{search_for}"
+    search_for = "foobar"
+    get(:search, params: {uname: search_for})
+    assert_redirected_to "/#{search_for}"
   end
 
   test "user input should be cleaned" do
-  	dirty_chars = ["!","@","%","&","\\","/","?","'","\"",";","=","<",">"]
-  	cleaned = @controller.clean "clean#{dirty_chars.join}me" 
-  	
-  	dirty_chars.each { |c|
-  	  assert !cleaned.include?(c), "string should NOT contain '#{c}'"	
-  	}
-  	assert cleaned == "cleanme"
+    dirty_chars = ["!","@","%","&","\\","/","?","'","\"",";","=","<",">"]
+    cleaned = @controller.clean "clean#{dirty_chars.join}me"
+
+    dirty_chars.each { |c|
+      assert !cleaned.include?(c), "string should NOT contain '#{c}'"
+    }
+    assert cleaned == "cleanme"
   end
 
   test "nonexistant path returns 404" do
-  	assert_raises ActionController::UrlGenerationError do
-  	  get "/no/such/path/"
-  	end
+    assert_raises ActionController::UrlGenerationError do
+      get "/no/such/path/"
+    end
   end
 
 end
