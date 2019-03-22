1357036f5965d59f6b976dbfa25fb24eac9658b0
Matthew Frost <Exupery@users.noreply.github.com>
2019-03-22 08:25:45 -0400

    removed deprecated secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Shouldifollow::Application.config.secret_token = "449c5ad234cb3f3f9c126118121f9b445fdae66b0d54c8b1642194c52f8facc19c9ccff689dae92f0a5a27d596f151704a9362867b18c7f74b9fc21743e57d8f"
 Shouldifollow::Application.config.secret_key_base = "15ba51827703b7f7609f7309829315699a3f11afb3e181b7b826600f736fa9ecb34cce69b60810d48d6469e66b2be9dbdf2d91fae98cb298d5227dee8453bc87"
